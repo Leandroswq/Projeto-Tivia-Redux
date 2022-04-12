4a490589d7d00746af5f63a5b29ad689b035a32c
Leandro Bonfim <leandrobonfim15@homtial.com>
2022-04-12 14:52:24 -0300

    Updates

---
+++ precache-manifest.263ab4a4a81882d2e4cbd8c7a23476ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924216728c84edffdb544ae65bf6b281",
+    "url": "/Projeto-Tivia-Redux/index.html"
+  },
+  {
+    "revision": "8f2e638dd33ef414ceac",
+    "url": "/Projeto-Tivia-Redux/static/css/main.953d6d51.chunk.css"
+  },
+  {
+    "revision": "31ce351e7ee079e407ef",
+    "url": "/Projeto-Tivia-Redux/static/js/2.76e0b40e.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Projeto-Tivia-Redux/static/js/2.76e0b40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2e638dd33ef414ceac",
+    "url": "/Projeto-Tivia-Redux/static/js/main.09b0beaf.chunk.js"
+  },
+  {
+    "revision": "d8cd69106f05511bb24d",
+    "url": "/Projeto-Tivia-Redux/static/js/runtime-main.0cd297d3.js"
+  }
+]);
